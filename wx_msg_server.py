e0c1f95c06fcd9c2d34251658d24205216e4a1be
Richard Liu <richard@kzkitchen.com>
2022-02-02 10:34:18 -0800

    update host IP to local

--- wx_msg_server.py
+++ wx_msg_server.py
@@ -514,8 +514,8 @@ def create_app():
     return app
 
 
-# if __name__ == '__main__':
-    # wechatserver = WeChatMsg(logger)
-    # wechatserver.server_run()
-    # serve(app,host="96.74.99.243", port=5000)
+if __name__ == '__main__':
+    wechatserver = WeChatMsg(logger)
+    wechatserver.server_run()
+    serve(app,host="0.0.0.0", port=5000)
     
