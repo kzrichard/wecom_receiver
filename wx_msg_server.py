bc681539c1c25571df7f489ab68f3be7cf6d801d
Richard Liu <richard@kzkitchen.com>
2022-02-02 12:57:29 -0800

    reduce the arg number

--- wx_msg_server.py
+++ wx_msg_server.py
@@ -609,7 +609,7 @@ def performance_review():
             from_user_id = xml_tree.find("FromUserName").text
             
             # use thread to save response time before timeout
-            Thread(target=wechatserver._send_performace_review_text_msg, args=(content, content, from_user_id, access_token)).start()
+            Thread(target=wechatserver._send_performace_review_text_msg, args=(content, from_user_id, access_token)).start()
             return '',200
         else:
             return
